6afef7f22709858824dd20619a023cf33b4320f0
AhmadMdi <admimohammadi@gmail.com>
2024-03-30 14:51:04 +0330

    set Repository pattern in project

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,9 +1,26 @@
 <?php
+
 namespace App\Services;
-use App\Http\Controllers\Controller;
+
+use App\Models\User;
 use Illuminate\Http\Request;
 
-interface UserService {
-    public function getAll();
-    public function addUser(Request $request);
+class UserServiceImpl implements UserService
+{
+    public function getAll(): \Illuminate\Database\Eloquent\Collection
+    {
+        return User::all();
+    }
+
+    public function addUser(Request $request): \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Builder
+    {
+        return User::query()->create([
+            'firstname' => $request->get('firstname'),
+            'lastname' => $request->get('lastname'),
+            'username' => $request->get('username'),
+            'nationalCode' => $request->get('nationalCode'),
+            'status' => $request->get('status'),
+            'password' => bcrypt($request->get('password')),
+        ]);
+    }
 }
