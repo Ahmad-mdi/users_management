5b636a85f35bda6534e83f0a7f1cdf07cffa5d2a
AhmadMdi <admimohammadi@gmail.com>
2024-04-05 02:09:35 +0330

    set status property or without

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $guarded = [];
+    protected $guarded = ['status']; //all property without status
 
     public function getRouteKeyName(): string
     {
