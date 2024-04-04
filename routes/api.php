591e9e57b94a6b4c39f787da159f977a3a2ff736
AhmadMdi <admimohammadi@gmail.com>
2024-04-05 02:13:54 +0330

    set changing api.php:
    1.set guest/auth route for users business

--- routes/api.php
+++ routes/api.php
@@ -5,21 +5,19 @@ use App\Http\Controllers\api\user\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-//routing for users****
-
-Route::group(['prefix' => '/users','middleware' => 'auth:sanctum'], function () {
-    Route::get('/',[UserController::class,'index']);
-    Route::get('/search',[UserController::class,'findByUsername']);
-    Route::post('/signup',[AuthController::class,'signup']);
-    Route::post('/logout',[AuthController::class,'logout']);
-    Route::put('/changePass',[AuthController::class,'changePassUser']);
-    Route::put('/{user}',[UserController::class,'update']);
-    Route::delete('/{user}',[UserController::class,'destroy']);
+//routing for users
+Route::group(['prefix' => '/users', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/search', [UserController::class, 'findByUsername']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::put('/changePass', [AuthController::class, 'changePassUser']);
+    Route::put('/{user}', [UserController::class, 'update']);
+    Route::delete('/{user}', [UserController::class, 'destroy']);
 });
 
-Route::group(['middleware'=>'guest:sanctum'],function (){
-    Route::post('/users/login',[AuthController::class,'login']);
+Route::group(['prefix' => '/users', 'middleware' => 'guest:sanctum'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/signup', [AuthController::class, 'signup']);
 });
-
-//end routing users****
+//end routing users ****************************************************************************************************
 
